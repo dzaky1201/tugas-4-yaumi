7a7491484405dfaea75eb8a0db73f53928f87ef7
dzaky1201 <dzakyhdr@gmail.com>
2021-10-19 17:40:15 +0700

    hapus file aja

--- backup.php
+++
@@ -1,7 +0,0 @@
-<?php foreach ($_SESSION['simpan_foto'] as $foto) { ?>
-    <?php if ($foto == "uploads/") { ?>
-        <img src="isi link default foto" alt="">
-    <?php } else { ?>
-        <img src="<?= $foto ?>" alt="">
-    <?php } ?>
-<?php } ?>
